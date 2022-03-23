16f20dc20a97683b2cca141ea0f20a7f7fa196e2
MasalovaL <lmasalova83@gmail.com>
2022-03-23 18:53:11 +0200

    Test1

---
+++ src/test/java/Main.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import javax.swing.text.html.HTMLEditorKit;
+import java.awt.*;
+
+public class Main {
+   @Test
+   public void test(){
+
+      System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
+      WebDriver driver = new ChromeDriver(new ChromeOptions().addArguments("--headless"));
+
+              driver.manage().window().maximize();
+              driver.get("http://automationpractice.com/");
+              driver.findElement(By.className("search_query")).sendKeys("summer"+ Keys.ENTER);
+              driver.findElement(By.id("selectProductSort")).click();
+              driver.findElement(By.xpath("//option[@value='price:desc']")).click();
+       //List<Integer>
+              driver.close();
+   }
+
+}
